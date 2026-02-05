43512ba47c3ae449b6eb33776e3e862bdba77f2e
Joao Marcos Carniel <joaomarcoscrr@gmail.com>
2026-02-05 21:28:31 +0000

    first

---
+++ PalindromeWithStack/PalindromeStack.cs
@@ -0,0 +1,48 @@
+﻿namespace PalindromeWithStack
+{
+    public class PalindromeStack
+    {
+        private Stack<char> _word;
+        private int _size;
+
+        public PalindromeStack()
+        {
+            _word = new Stack<char>();
+        }
+
+        public void Push(char c)
+        {
+            _word.Push(c);
+            _size++;
+        }
+
+        public char Pop() 
+        {
+            if (!IsEmpty())
+            {
+                _size--;
+                return _word.Pop();
+            }
+            throw new Exception("Stack is empty");
+        }
+
+        public bool IsEmpty()
+        {
+            return _size == 0;
+        }
+
+
+        public bool IsPalindrome(string word)
+        {
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] != Pop())
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ PalindromeWithStack/Program.cs
@@ -0,0 +1,34 @@
+﻿using PalindromeWithStack;
+
+class Program
+{
+    static void Main()
+    {
+        PalindromeStack stack = new PalindromeStack();
+        while (true)
+        {
+            Console.WriteLine("=******=");
+            Console.WriteLine("=******=");
+            Console.WriteLine("Welcome to the Palindrome Checker Program");
+            Console.WriteLine("Write a word: ");
+            var word = Console.ReadLine();
+            if (word != null)
+            {
+                word = word.ToLower();
+                foreach (char i in word)
+                {
+                    stack.Push(i);
+                }
+
+                var isPalindrome = stack.IsPalindrome(word);
+                Console.WriteLine($"The word {word} is a Palindrome? --->>> {isPalindrome}");
+            }
+
+            Console.WriteLine("Close Program? [y/n]");
+            if(Console.ReadLine() == "y")
+            {
+                break;
+            }
+        }
+    }
+}
